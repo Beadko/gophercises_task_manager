502e5377c8ccee5906ea72880ec949f15b00859a
Beadko <beadko@gmail.com>
2025-04-09 16:17:20 +0700

    Create a bucket

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/Beadko/gophercises_task_manager
 
 go 1.23.1
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/spf13/cobra v1.9.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/Beadko/gophercises_task_manager/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/Beadko/gophercises_task_manager/cmd"
+	"github.com/Beadko/gophercises_task_manager/db"
+)
 
 func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Could not get the user home directory: %v\n", err)
+		return
+	}
+	dbPath := filepath.Join(home, "tasks.db")
+	if err := db.Init(dbPath); err != nil {
+		fmt.Printf("Could not initialise the database: %v\n", err)
+		return
+	}
 	cmd.Execute()
 }
