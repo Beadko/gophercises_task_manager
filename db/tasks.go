613138ea3044e3d0534c6ab7eab6cc513d4906d8
Beadko <beadko@gmail.com>
2025-04-11 15:45:30 +0700

    Store tasks in the bucket

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -26,4 +27,29 @@ func Init(dbpath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func CreateTask(t string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		return b.Put(itob(id), []byte(t))
+	})
+	if err != nil {
+		return -1, nil
+	}
+
+	return id, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
