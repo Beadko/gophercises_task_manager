f0615d4d95945310577a48eb8c6c9b7442ba41d6
Beadko <beadko@gmail.com>
2025-04-12 14:47:23 +0700

    Finalise list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/Beadko/gophercises_task_manager/db"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +13,19 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all of your tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Something went wrong:%v\n", err)
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have completed all tasks. Well done! 😎")
+			return
+		}
+		fmt.Println("You have the following tasks:")
+		for i, t := range tasks {
+			fmt.Printf("%d. %s\n", i+1, t.Value)
+		}
 	},
 }
 
